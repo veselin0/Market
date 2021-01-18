51a5e62bd2ce12cdce325a1e47eeca6570eb13e1
Veselin <vesselin60@gmail.com>
2021-01-18 12:14:29 +0200

    Built and working

--- src/components/common/Todo.js
+++ src/components/common/Todo.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { List } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 function Todo({ id, title, complete }) {
   async function toggleComplete() {
@@ -9,15 +11,40 @@ function Todo({ id, title, complete }) {
     });
   }
 
+  async function del() {
+    await firestore().collection('todos').doc(id).delete();
+  }
+
   return (
-    <List.Item
-      title={title}
-      onPress={() => toggleComplete()}
-      left={(props) => (
-        <List.Icon {...props} icon={complete ? 'check' : 'cancel'} />
-      )}
-    />
+    <View style={styles.container}>
+      <View style={styles.listStyle}>
+        <List.Item
+          title={title}
+          onPress={() => toggleComplete()}
+          left={(props) => (
+            <List.Icon {...props} icon={complete ? 'check' : 'cancel'} />
+          )}
+        />
+      </View>
+      <TouchableOpacity style={styles.iconStyle} onPress={() => del()}>
+        <Icon name="trash" size={30} />
+      </TouchableOpacity>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listStyle: {
+    flex: 8,
+  },
+  iconStyle: {
+    flex: 1,
+  },
+});
 
 export default React.memo(Todo);
--- src/components/common/Todos.js
+++ src/components/common/Todos.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import { Appbar, TextInput, Button } from 'react-native-paper';
@@ -14,8 +14,12 @@ function Todos() {
   const [todos, setTodos] = useState([]);
 
   async function addTodo() {
-    await ref.add({ title: todo, complete: false });
-    setTodo('');
+    if (todo) {
+      await ref.add({ title: todo, complete: false });
+      setTodo('');
+    } else {
+      Alert.alert('Попълни полето!');
+    }
   }
 
   useEffect(() => {
@@ -50,7 +54,7 @@ function Todos() {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <Todo {...item} />}
       />
-      <TextInput label={'Gocho Todo'} value={todo} onChangeText={setTodo} />
+      <TextInput label={'Трябва ни ...'} value={todo} onChangeText={setTodo} />
       <Button onPress={() => addTodo()}>добави покупка</Button>
       <Button onPress={() => auth().signOut()}>излез!</Button>
     </>
